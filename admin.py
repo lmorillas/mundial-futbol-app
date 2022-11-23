6c1f039af6e90645b85173ddfefeb7c1e4fb399b
lmorillas <morillas@gmail.com>
2022-11-23 12:18:59 +0100

    update

--- admin.py
+++ admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+
 from .models import Jugador
 
 # Register your models here.
 
-admin.site.register(Jugador)
+@admin.register(Jugador)
+class JugadorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'posicion', 'img_imagen')
+    list_filter = ('posicion', 'equipo')
+    search_fields = ('nombre', 'apellido', 'posicion')
+
+ 
--- models.py
+++ models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
@@ -9,5 +12,26 @@ class Jugador(models.Model):
     imagen = models.URLField(blank=True)
     posicion = models.CharField(max_length=100)
     equipo = models.CharField(max_length=100)
+
+    def __str__(self):
+        if self.apellido:
+            return f'{self.nombre} {self.apellido}'
+        return self.nombre
+
+    @admin.display(description=_("Image"))
+    def img_imagen(self):
+        if self.imagen:
+            return format_html(
+                '<img src="{}" width="80" />'.format(
+                    self.imagen)
+            )
+        else:
+            return ''
+
+    class Meta:
+        verbose_name = _("Player")
+        verbose_name_plural = _("Players")
+        ordering = ('-id',)
+
     
 
